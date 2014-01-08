922f0df00097de824d59884ad384f32c28f44400
bertt <btemme@hotmail.com>
2014-01-09 00:03:28 +0100

    initial

---
+++ CacheOutputTest/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace CacheOutputTest
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RouteTable.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new { id = RouteParameter.Optional }
+            );
+            var htmlMediaTypeFormatter = new HtmlMediaTypeFormatter();
+            GlobalConfiguration.Configuration.Formatters.Add(htmlMediaTypeFormatter);
+        }
+
+    }
+}
---
+++ CacheOutputTest/Test2Controller.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CacheOutputTest
+{
+    public class Test2Controller:ApiController
+    {
+        public HttpResponseMessage GetTest()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK,
+                "test");
+        }
+    }
+}
