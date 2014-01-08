11270c286de0a2a57dbccec045129c8017f6e5d6
bertt <btemme@hotmail.com>
2014-01-08 23:49:07 +0100

    initital

---
+++ CacheOutputTest/HtmlMediaTypeFormatter.cs
@@ -0,0 +1,44 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace CacheOutputTest
+{
+    public class HtmlMediaTypeFormatter : MediaTypeFormatter
+    {
+        public HtmlMediaTypeFormatter()
+        {
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        }
+
+        public override Task WriteToStreamAsync(Type type, object value, Stream writeStream,
+            System.Net.Http.HttpContent content, TransportContext transportContext)
+        {
+            var taskSource = new TaskCompletionSource<object>();
+            var doc = new HtmlDocument();
+            var htmlElement = doc.CreateElement("html");
+            doc.DocumentNode.AppendChild(htmlElement);
+
+            htmlElement.InnerHtml = "html:" + (string) value;
+            doc.Save(writeStream);
+            taskSource.SetResult(null);
+            return taskSource.Task;
+        }
+
+        public override bool CanReadType(Type type)
+        {
+            return false;
+        }
+
+        public override bool CanWriteType(Type type)
+        {
+            return (
+                type == typeof(string)
+                    );
+        }
+    }
+}
---
+++ CacheOutputTest/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.OutputCache;
+
+namespace CacheOutputTest
+{
+    public class TestController:ApiController
+    {
+        [CacheOutput(ClientTimeSpan = 1000, ServerTimeSpan = 1000)]
+        public HttpResponseMessage GetTest()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK,
+                "test");
+        }
+    }
+}
