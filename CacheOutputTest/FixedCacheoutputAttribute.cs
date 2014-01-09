02b8b07b8168d9a2a49bd63da02e337e0ea3c9a4
bertt <btemme@hotmail.com>
2014-01-09 21:32:22 +0100

    added test3

---
+++ CacheOutputTest/FixedCacheoutputAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using WebAPI.OutputCache;
+
+namespace CacheOutputTest
+{
+    public class FixedCacheOutput : CacheOutputAttribute
+    {
+        private static MediaTypeHeaderValue _defaultMediaType = new MediaTypeHeaderValue("application/json");
+
+        protected override MediaTypeHeaderValue GetExpectedMediaType(HttpConfiguration config, HttpActionContext actionContext)
+        {
+            MediaTypeHeaderValue responseMediaType = null;
+
+            var negotiator = config.Services.GetService(typeof(IContentNegotiator)) as IContentNegotiator;
+            var returnType = actionContext.ActionDescriptor.ReturnType;
+
+            if (negotiator != null && returnType != typeof(HttpResponseMessage))
+            {
+                var negotiatedResult = negotiator.Negotiate(returnType, actionContext.Request, config.Formatters);
+                responseMediaType = negotiatedResult.MediaType;
+            }
+            else
+            {
+                if (actionContext.Request.Headers.Accept != null)
+                {
+                    responseMediaType = actionContext.Request.Headers.Accept.FirstOrDefault();
+                    if (responseMediaType == null ||
+                        !config.Formatters.Any(x => x.SupportedMediaTypes.Contains(responseMediaType)))
+                    {
+                        return _defaultMediaType;
+                    }
+                }
+            }
+
+            return responseMediaType;
+        }
+    }
+}
---
+++ CacheOutputTest/Test3Controller.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CacheOutputTest
+{
+    public class Test3Controller:ApiController
+    {
+        [FixedCacheOutput(ClientTimeSpan = 1000, ServerTimeSpan = 1000)]
+        public HttpResponseMessage GetTest()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK,
+                "test");
+        }
+
+    }
+}
